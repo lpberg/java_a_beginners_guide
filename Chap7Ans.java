742325836dfe28d00fd3f8955064aca0006709ce
Leif Berg <leif.berg@gmail.com>
2016-11-05 20:14:38 -0600

    init commit

---
+++ Chap7Ans.java
@@ -0,0 +1,25 @@
+package java_a_beginners_guide;
+
+//listening 1
+// A subclass of TwoDShape for circles. 
+class Circle extends TwoDShape { 
+  // A default constructor.
+  Circle() {
+    super();
+  }
+
+  // Construct Circle
+  Circle(double x) {
+    super(x, "circle"); // call superclass constructor
+  }
+
+  // Construct an object from an object.
+  Circle(Circle ob) {
+    super(ob); // pass object to TwoDShape constructor
+  }
+   
+  double area() { 
+    return (getWidth() / 2) * (getWidth() / 2) * 3.1416;
+  } 
+}
+
